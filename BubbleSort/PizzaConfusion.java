4d35da655423a567adf15f88edad94f67d60bb74
Abhinav <abhi2000.rtk@gmail.com>
2020-07-31 16:39:52 +0530

    Concept - lexicographical sorting

--- BubbleSort/PizzaConfusion.java
+++ BubbleSort/PizzaConfusion.java
@@ -1,5 +1,37 @@
 package HackerEarth.BubbleSort;
 
+import java.util.Scanner;
+
 public class PizzaConfusion {
 
+	public static void main(String[] args) {
+
+		Scanner scn = new Scanner(System.in);
+
+		int size = scn.nextInt();
+
+		String[] restro = new String[size];
+		int[] scores = new int[size];
+		
+		int max  = Integer.MIN_VALUE;
+		int idx = -1;
+		
+		for(int i=0;i<size;i++) {
+			restro[i]=scn.next();
+			scores[i]=scn.nextInt();
+			if(scores[i]>max) {
+				max = scores[i];
+				idx=i;
+			}else if(scores[i]==max) {
+				//sort lexicographically
+				int curr = i;
+				int prev = idx;
+				if (restro[prev].compareTo(restro[curr]) > 0) {
+					idx=curr;
+                }
+			}
+		}
+
+		System.out.println(restro[idx]);
+	}
 }
